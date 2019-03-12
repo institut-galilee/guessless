55d118303b7d815723067d0c3fb4178636b6689e
maherlaaroussi <maher@outlook.fr>
2019-03-12 16:28:31 +0100

    Add function word

--- src/wiki.py
+++ src/wiki.py
@@ -1,10 +1,20 @@
 import requests
 import json
 
+def word(word) :
+	response = requests.get("https://fr.wikipedia.org/w/api.php?format=json&action=opensearch&search=" + word)
+	suggestions = json.loads(response.content)
+	return suggestions[1][0]
+
 def search(name) :
-    response = requests.get("https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + name)
+    response = requests.get("https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + word(translate(name)))
     if (response.status_code == 200):
         data = json.loads(response.content)
         for key in data['query']['pages']:
             if (key != '-1'):
                 print(data['query']['pages'][key]['extract'])
+
+def translate(word) :
+	return word
+
+search("smartphone")

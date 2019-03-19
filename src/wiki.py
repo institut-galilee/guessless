21c9e1c7a28b01134cfdf6dab21e1ba052e401f1
maherlaaroussi <maher@outlook.fr>
2019-03-19 15:06:31 +0100

    Début code partie graphique

--- src/wiki.py
+++ src/wiki.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def word(word) :
-	response = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=" + word)
+	response = requests.get("https://fr.wikipedia.org/w/api.php?format=json&action=opensearch&search=" + word)
 	suggestions = json.loads(response.content)
 	if (suggestions[0] != Null):
 		return suggestions[0]
@@ -10,7 +10,7 @@ def word(word) :
 		return suggestions[1][0]
 
 def search(name) :
-    response = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + translate(name))
+    response = requests.get("https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + translate(name))
     if (response.status_code == 200):
         data = json.loads(response.content)
         for key in data['query']['pages']:
@@ -19,3 +19,5 @@ def search(name) :
 
 def translate(word) :
 	return word
+
+search("pomme")

aaa9a0a409876b4a018040fa8ffaa4b4c87876fd
maherlaaroussi <maher@outlook.fr>
2019-03-22 16:53:41 +0100

    Add wiki.py

---
+++ src/wiki.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# Get the correct word from suggestions
+def word(word) :
+	response = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=" + word)
+	suggestions = json.loads(response.content)
+	try:
+		return suggestions[1][0]
+	except IndexError:
+		return suggestions[0]
+
+# Get the description of the word
+def search(name) :
+    response = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + word(name))
+    if (response.status_code == 200):
+        data = json.loads(response.content)
+        for key in data['query']['pages']:
+            if (key != '-1'):
+                return data['query']['pages'][key]['extract']

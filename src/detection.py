2ac092ab232715e521dcba15634760f34a4faaa9
maherlaaroussi <maher@outlook.fr>
2019-05-02 19:44:43 +0200

    Structuration du code

---
+++ src/detection.py
@@ -0,0 +1,69 @@
+import os
+import cv2
+import numpy as np
+from picamera import PiCamera
+import tensorflow as tf
+import sys
+
+def init_detection():
+    min_score = 0.5
+    IM_WIDTH = 640
+    IM_HEIGHT = 480
+
+    sys.path.append('..')
+    from utils import label_map_util
+    MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
+    CWD_PATH = os.getcwd()
+    PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, 'frozen_inference_graph.pb')
+    PATH_TO_LABELS = os.path.join(CWD_PATH, 'data', 'mscoco_label_map.pbtxt')
+    NUM_CLASSES = 90
+    label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+    categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+    category_index = label_map_util.create_category_index(categories)
+    detection_graph = tf.Graph()
+
+    # Load Tensorflow into memory
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        sess = tf.Session(graph=detection_graph)
+
+    # Ressources
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+    detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    camera = PiCamera()
+    camera.resolution = (IM_WIDTH, IM_HEIGHT)
+    camera.framerate = 10
+
+def close():
+    camera.close()
+    cv2.destroyAllWindows()
+
+
+def detect(self):
+    # Capture an image & expand it
+    camera.capture("image.png")
+    image = cv2.imread("image.png")
+    image_expanded = np.expand_dims(image, axis=0)
+
+    # Detection
+    (boxes, scores, classes, num) = sess.run(
+    [detection_boxes, detection_scores, detection_classes, num_detections],
+    feed_dict={image_tensor: image_expanded})
+
+    objects = []
+    for index, value in enumerate(classes[0]):
+        object_dict = {}
+        if scores[0, index] > 0:
+            object_dict[(category_index.get(value)).get('name')] = scores[0, index]
+            objects.append(object_dict)
+    print (objects)
+
+    return objects

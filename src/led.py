69f61a5315468eb1336133293fd19742ad2ec31a
HamidOuF <vilspy@gmail.com>
2019-05-06 20:20:03 +0200

    Add start function led

---
+++ src/led.py
@@ -0,0 +1,5 @@
+where_path = "cd /home/pi/Documents/rpi-rgb-led-matrix/utils/ && "
+params = " --led-rows=32 --led-cols=64 --led-no-hardware-pulse"
+
+def start() :
+    os.system(where_path + "sudo ./video-viewer videoLogo.webm" + params)

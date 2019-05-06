b8853d9508ffc9e74b53feb3abb1f9343904702a
HamidOuF <vilspy@gmail.com>
2019-05-06 21:35:00 +0200

    Change color to white

--- libs/samples/pulsing-brightness.py
+++ libs/samples/pulsing-brightness.py
@@ -9,23 +9,15 @@ class GrayscaleBlock(SampleBase):
     def run(self):
         max_brightness = self.matrix.brightness
         count = 0
-        c = 255
 
-        while (True):
+        self.matrix.Fill(255, 255, 255)
+
+        while (300):
             if self.matrix.brightness < 1:
                 self.matrix.brightness = max_brightness
                 count += 1
             else:
                 self.matrix.brightness -= 1
-
-            if count % 4 == 0:
-                self.matrix.Fill(c, 0, 0)
-            elif count % 4 == 1:
-                self.matrix.Fill(0, c, 0)
-            elif count % 4 == 2:
-                self.matrix.Fill(0, 0, c)
-            elif count % 4 == 3:
-                self.matrix.Fill(c, c, c)
 
             self.usleep(20 * 1000)
 
--- src/led.py
+++ src/led.py
@@ -1,3 +1,5 @@
+import os
+
 where_path = "cd /home/pi/Documents/rpi-rgb-led-matrix/"
 params = " --led-rows=32 --led-cols=64 --led-no-hardware-pulse"
 params_py = " --led-rows 32 --led-cols 64 --led-no-hardware-pulse NO_HARDWARE_PULSE"
@@ -6,4 +8,4 @@ def start() :
     os.system(where_path + "utils && sudo ./video-viewer videoLogo.webm" + params)
 
 def guess() :
-    os.system(where_path + "binding/python/samples && sudo python ./pulsing-brightness.py " + params_py)
+    os.system(where_path + "bindings/python/samples && sudo python ./pulsing-brightness.py " + params_py)

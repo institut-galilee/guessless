4cf6c80b6d776d89cdb24ab4021fe2bd1e07e94f
HamidOuF <vilspy@gmail.com>
2019-05-06 20:22:14 +0200

    Add guess function led

--- src/led.py
+++ src/led.py
@@ -3,3 +3,6 @@ params = " --led-rows=32 --led-cols=64 --led-no-hardware-pulse"
 
 def start() :
     os.system(where_path + "sudo ./video-viewer videoLogo.webm" + params)
+
+def guess() :
+    os.system(where_path + "sudo ./video-viewer guess.webm" + params)

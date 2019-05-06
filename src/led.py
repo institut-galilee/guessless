c1d294303b345774e08869de416531d1809b8d87
HamidOuF <vilspy@gmail.com>
2019-05-06 21:10:53 +0200

    Missing fix

--- src/led.py
+++ src/led.py
@@ -1,8 +1,9 @@
-where_path = "cd /home/pi/Documents/rpi-rgb-led-matrix/utils/ && "
+where_path = "cd /home/pi/Documents/rpi-rgb-led-matrix/"
 params = " --led-rows=32 --led-cols=64 --led-no-hardware-pulse"
+params_py = " --led-rows 32 --led-cols 64 --led-no-hardware-pulse NO_HARDWARE_PULSE"
 
 def start() :
-    os.system(where_path + "sudo ./video-viewer videoLogo.webm" + params)
+    os.system(where_path + "utils && sudo ./video-viewer videoLogo.webm" + params)
 
 def guess() :
-    os.system(where_path + "sudo ./video-viewer guess.webm" + params)
+    os.system(where_path + "binding/python/samples && sudo python ./pulsing-brightness.py " + params_py)

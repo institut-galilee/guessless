5c6ee06d48cbb2ba955eb10b9b3dd72f9165965d
maherlaaroussi <maher@outlook.fr>
2019-05-06 17:19:28 +0200

    Test getting names

--- src/wiki.py
+++ src/wiki.py
@@ -12,7 +12,7 @@ def word(word) :
 def search(name) :
     response = requests.get("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=0&titles=" + name)
     if (response.status_code == 200):
-        data = json.loads(response.content)
+        data = json.loads(response.content.decode('utf-8'))
         for key in data['query']['pages']:
             if (key != '-1'):
                 return data['query']['pages'][key]['extract']

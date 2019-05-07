94905d86e549c2cfe36487483047568b9753a633
maherlaaroussi <maher@outlook.fr>
2019-05-07 08:24:18 +0200

    Add bye function to led

--- src/led.py
+++ src/led.py
@@ -5,7 +5,10 @@ params = " --led-rows=32 --led-cols=64 --led-no-hardware-pulse"
 params_py = " --led-rows 32 --led-cols 64 --led-no-hardware-pulse NO_HARDWARE_PULSE"
 
 def start() :
-    os.system(where_path + "utils && sudo ./video-viewer videoLogo.webm" + params)
+    os.system(where_path + "utils && sudo ./video-viewer logo.webm" + params)
 
 def guess() :
     os.system(where_path + "bindings/python/samples && sudo python ./pulsing-brightness.py " + params_py)
+
+def bye() :
+    os.system(where_path + "utils && sudo ./video-viewer logo.webm" + params)
